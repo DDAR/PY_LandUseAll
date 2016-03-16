a3f4098524965f4dd24b7e3e847ce56f9de0939f
DDA <donna.arnett@co.yakima.wa.us>
2016-03-16 09:49:58 -0700

    Changed File References

--- ReportMoxee.py
+++ ReportMoxee.py
@@ -86,10 +86,10 @@ if __name__ == '__main__':
 
     # Script tool params
 
-    SummaryLayer = r'R:\disk_5\projects\county\planning\uga_analysis\moxee\Analysis_2015\MoxeeAnalysis2015.gdb\MoxeeUGA_090115'
+    SummaryLayer = r'R:\disk_5\projects\county\planning\uga_analysis\moxee\Analysis_2015\MoxeeAnalysis2015.gdb\MoxeeUGAProp_031616'
     baseFileLocation = r'R:\disk_5\projects\county\planning\uga_analysis\moxee\Analysis_2015'
     fileGeoLocation = r'R:\disk_5\projects\county\planning\uga_analysis\moxee\Analysis_2015\MoxeeAnalysis2015.gdb'
-    reportFile = os.path.join(baseFileLocation, "MoxeeReport_090115.txt")
+    reportFile = os.path.join(baseFileLocation, "MoxeeReport_031616.txt")
     print reportFile
     value_field = 'ACRES'
     value_vacant = 'ACRES_VAC'
